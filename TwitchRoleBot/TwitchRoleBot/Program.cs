ee2960a70ab1d22d257351983624fb18b7496193
duna-one <korotkovdan@gmail.com>
2024-04-09 19:36:39 +0300

    Add Serilog

--- TwitchRoleBot/TwitchRoleBot/Program.cs
+++ TwitchRoleBot/TwitchRoleBot/Program.cs
@@ -1,6 +1,15 @@
+using Serilog;
 using TwitchRolesBot.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
